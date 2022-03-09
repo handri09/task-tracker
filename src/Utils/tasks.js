3660673262f20876efe17251efde3f770c41d4e5
Hajaniaina Andrianavalona <hajaniaina.andri@gmail.com>
2022-03-09 22:57:10 +0300

    [code] Worked on the data tasks

--- src/Utils/tasks.js
+++ src/Utils/tasks.js
@@ -1,10 +1,10 @@
 export const tasks = [
-  { name:'task - 01', action: 'do task - 01', date: '2022-03-10', status: 'Done' },
-  { name:'task - 02', action: 'do task - 02', date: '2022-03-20', status: 'Ongoing' },
-  { name:'task - 03', action: 'do task - 03', date: '2022-03-30', status: 'On Hold' },
-  { name:'task - 04', action: 'do task - 04', date: '2022-04-10', status: 'Cancelled' },
-  { name:'task - 05', action: 'do task - 05', date: '2022-04-15', status: 'New' },
-  { name:'task - 06', action: 'do task - 06', date: '2022-05-01', status: 'Ongoing' }
+  { name:'task - 01', action: 'do task - 01', date: '2022-03-10', type: "Contract", status: 'Done' },
+  { name:'task - 02', action: 'do task - 02', date: '2022-03-20', type: "Amendment", status: 'Ongoing' },
+  { name:'task - 03', action: 'do task - 03', date: '2022-03-30', type: "PO", status: 'On Hold' },
+  { name:'task - 04', action: 'do task - 04', date: '2022-04-10', type: "Quotation", status: 'Cancelled' },
+  { name:'task - 05', action: 'do task - 05', date: '2022-04-15', type: "Contract", status: 'New' },
+  { name:'task - 06', action: 'do task - 06', date: '2022-05-01', type: "Contract", status: 'Ongoing' }
 ]
 
 export const tasks_ = [
