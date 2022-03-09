a8ac1644bf87352f865cebdccf9ca8e0999b6475
Hajaniaina Andrianavalona <hajaniaina.andri@gmail.com>
2022-03-09 16:14:02 +0300

    [code] Add all details in the Modal

--- src/Utils/tasks.js
+++ src/Utils/tasks.js
@@ -6,3 +6,33 @@ export const tasks = [
   { name:'task - 05', action: 'do task - 05', date: '2022-04-15', status: 'New' },
   { name:'task - 06', action: 'do task - 06', date: '2022-05-01', status: 'Ongoing' }
 ]
+
+export const tasks_ = [
+  {
+    description: "task - 01",  // Task Name 
+    status:"Ongoing",         // Done, Ongoing, On Hold, Cancelled, New
+    type:"Contract",          // Contract, Amendment, PO, Quotation 
+    strategy:"Tendering",     // Tendering, SSJ
+    reference:"",                
+    tracked: false,               // True, False
+    endUser : { name: "Engineering", departement: "Vikesh" }, // End-User Name - Department
+    state:"Revision of T&C",
+    nextAction:"Action - 01",
+    responsible:["Diary"],
+    sowSrfReceived:"",
+    frqSent:"",
+    siteVisit:"",
+    frqClosed:"",
+    extendedClosingDate:"",
+    quotationSentToEu:"",
+    quoteClarif:"",
+    tbe:"",
+    cbe:"",
+    rtaSub:"",
+    rtaValid:"",
+    execDate:"",
+    budget:"",
+    saving:"",
+    nextStep:"",
+    bidders:"DRA_FLUOR_SNC"
+  }]
